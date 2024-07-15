478ba5f97af63df73419d5ac2bd8552ea6b58881
tomiputra <tomi.mandala@privyid.tech>
2024-07-15 10:58:13 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Tomi Mandala Putra on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
